fbed60857c56dd8c24153c30c7fab67b7ac5cd37
Anastasiia <vostrova_1994@mail.ru>
2022-03-02 17:49:21 +0300

    HW3-Cycles

---
+++ HW3-12/HW3-12/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+Console.WriteLine("Введите первое число");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int B = Convert.ToInt32(Console.ReadLine());
+bool check = false;
+
+while (A != 0)
+{
+    int tmp = A % 10;
+    while (B != 0)
+    {
+        int tMppp = B % 10;
+        if (tmp == tMppp) 
+        {
+            check = true;
+        }
+        B = B / 10;
+    }
+    A = A / 10;
+}
+    if (check == true)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+
+
+
+
